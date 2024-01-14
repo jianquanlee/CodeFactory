17eac9181a03f39424f907b3b80be9d03e41b591
JianquanLi <973512149@qq.com>
2024-01-14 23:33:24 +0800

    [Update] 新增Python3数据处理函数 - 获取CSV文件的编码格式

--- Python3/DataProcess/utils.py
+++ Python3/DataProcess/utils.py
@@ -18,3 +18,17 @@ class DataProcess:
             return True
         else:
             return False
+
+    @staticmethod
+    def get_csv_encoding(file_path: str) -> str:
+        """
+        获取csv文件的编码格式
+        :param file_path: csv文件的位置
+        :return: csv文件的编码格式
+        """
+        import chardet
+
+        with open(file_path, 'rb') as f:
+            rawdata = f.read()
+            result = chardet.detect(rawdata)
+            return result['encoding']
